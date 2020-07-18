9bad630f77f7b6a88bf88a3db6da0c5ce8f63046
Rodrigo Bighetti <robighetti@gmail.com>
2020-07-18 19:10:01 -0300

    inicio do dashboard

--- web/src/pages/Dashboard/index.tsx
+++ web/src/pages/Dashboard/index.tsx
@@ -1,11 +1,40 @@
 import React from 'react';
+import { FiEdit, FiLogOut } from 'react-icons/fi';
 
-import { Container } from './styled';
+import Button from '../../components/Button';
+import PetList from '../PetList';
+
+import { Container, Content, Header, Menu } from './styled';
 
 const Dashboard: React.FC = () => {
   return (
     <Container>
-      <h1>Dashboard</h1>
+      <Content>
+        <Header>
+          <strong>Rodrigo Bighetti</strong>
+          <a href="#" onClick={() => {}}>
+            <FiEdit />
+            Meu perfil
+          </a>
+          <img
+            src="https://ui-avatars.com/api/?font-size=0.40&background=0030ff&color=fff&name=Rodrigo Bighetti"
+            alt="Rodrigo Bighetti"
+          />
+
+          <span>Piracicaba - São Paulo</span>
+          <hr />
+        </Header>
+
+        <Menu>
+          <Button>Cadastrar Pet</Button>
+          <Button>
+            <FiLogOut />
+            Sair
+          </Button>
+        </Menu>
+      </Content>
+
+      <PetList />
     </Container>
   );
 };
--- web/src/pages/Dashboard/styled.ts
+++ web/src/pages/Dashboard/styled.ts
@@ -1,3 +1,79 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  position: relative;
+  background: #fff;
+  height: 100vh;
+  width: 100%;
+
+  display: flex;
+`;
+
+export const Content = styled.div`
+  flex: 0 0 20%;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Header = styled.div`
+  padding: 8px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  strong {
+    margin-top: 36px;
+    font-size: 36px;
+  }
+
+  a {
+    margin-top: 8px;
+    text-decoration: none;
+    display: flex;
+    transition: all 0.2s;
+    color: #0030ff;
+
+    &:hover {
+      transform: translateY(-3px);
+    }
+
+    svg {
+      margin-right: 8px;
+    }
+  }
+
+  img {
+    margin: 36px 0;
+    width: 150px;
+    height: 150px;
+    border-radius: 50%;
+  }
+
+  span {
+    font-size: 18px;
+    font-weight: 600;
+    text-transform: uppercase;
+  }
+
+  hr {
+    margin-top: 8px;
+    width: 90%;
+  }
+`;
+
+export const Menu = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  button {
+    width: 90%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    svg {
+      margin-right: 16px;
+    }
+  }
+`;
